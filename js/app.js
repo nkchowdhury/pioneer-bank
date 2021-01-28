c9b328691893cdba8474002098e8a79bedb13a39
Nazmul Karim Chowdhury <nkchowdhury627@gmail.com>
2021-01-28 09:02:12 -0800

    new add

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById('login').addEventListener('click', function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
+
+});
